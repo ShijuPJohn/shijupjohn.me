84c046f15d0cfb9378db547d8bd0a8f64311f49f
ShijuPJohn <shijupjohn4u@gmail.com>
2022-04-05 21:38:41 +0530

    feat: add _document file and a responsive appbar. To be removed later

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import Layout from '../src/components/Layout'
+import React from 'react'
 
 export default function Home() {
   return (
+   <React.Fragment>
+      <Head>
+    <title>SHIJU P JOHN</title>
+  </Head>
      <Layout>
        <h1>Hello</h1>
      </Layout>
+   </React.Fragment>
   )
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,19 @@
 import React from "react"
-import Head from "next/head"
-function Layout(props){
-return(
-    <React.Fragment>
-         <Head>
-        <title>SHIJU P JOHN</title>
-        <meta name="description" content="Portfolio site of Shiju P John" />
-        <link rel="icon" href={'/favicon.jpg'} />
-      </Head>
-      <main >
-      {props.children}
-      </main>
+import ResponsiveAppBar from "./ResponsiveAppBar";
 
-      <footer >
-       
-      </footer>
-    </React.Fragment>
-)
+function Layout(props) {
+    return (
+        <React.Fragment>
+            <ResponsiveAppBar/>
+            <main>
+                {props.children}
+            </main>
+
+            <footer>
+
+            </footer>
+        </React.Fragment>
+    )
 }
+
 export default Layout
