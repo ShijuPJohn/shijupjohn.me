1309b153c84ffb282a244a304263e9be45f023a0
ShijuPJohn <shijupjohn4u@gmail.com>
2022-04-09 21:55:24 +0530

    feat: add a responsive icon and menu

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
-import React from 'react'
+import React, {useState} from 'react'
 import {createUseStyles} from "react-jss";
 import {faFacebookF, faLinkedinIn, faTwitter} from "@fortawesome/free-brands-svg-icons";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import Image from "next/image";
-import LogoImage from "../public/logo-01.jpg"
+import {Icon, useMediaQuery, useTheme} from "@mui/material";
+import {faXmark} from "@fortawesome/free-solid-svg-icons/faXmark";
+import ClearIcon from '@mui/icons-material/Clear';
 
 const useStyles = createUseStyles({
     root_container: {
@@ -17,9 +18,10 @@ const useStyles = createUseStyles({
         height: '100vh',
     },
     side_nav_content: {
+        width: 'calc(3vw + 3vh)',
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
         alignItems: 'center',
         height: '100vh',
         backgroundColor: 'var(--primary-color)'
@@ -27,14 +29,15 @@ const useStyles = createUseStyles({
     side_nav_content_logo: {
         width: 'calc(5vw + 3vh)',
         position: 'relative',
+        backgroundColor: "blue",
     },
     logo_image: {},
     social_ul: {},
     social_button: {
         display: 'block',
         color: '#d1d1d1',
-        margin: '2rem 0',
-        fontSize: 'calc(1rem + 1.3vw)',
+        margin: 'calc(2rem + .7vw) 0',
+        fontSize: 'calc(1rem + 1.1vw)',
         transition: '.2s',
         "&:hover": {
             color: '#ded18a',
@@ -58,8 +61,27 @@ const useStyles = createUseStyles({
         justifyContent: "center",
 
     },
-    main_nav_hamburger: {},
-    line: {},
+    main_nav_hamburger: {
+        width: "5rem",
+        height: "5rem",
+        position: "fixed",
+        top: "2rem",
+        right: "3rem",
+        backgroundColor: "var(--secondary-color)",
+        cursor: "pointer",
+        borderRadius: "50%",
+        padding: "1rem",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    line: {
+        width: "2rem",
+        backgroundColor: "white",
+        height: ".3rem",
+        margin: ".3rem",
+    },
     line1: {},
     line2: {},
     line3: {},
@@ -72,7 +94,7 @@ const useStyles = createUseStyles({
         color: "white",
     },
     nav_btn: {
-        fontSize: "calc(1rem + .667vw)",
+        fontSize: "calc(1.2rem + .667vw)",
         margin: "1rem calc(1rem + 1vw)",
         position: "relative",
         '&::after': {
@@ -80,43 +102,95 @@ const useStyles = createUseStyles({
             position: "absolute",
             backgroundColor: "white",
             bottom: "0",
-            left:"0",
+            left: "0",
             width: "100%",
             height: ".25rem",
             display: "inline-block",
             transform: "scaleX(0)",
-            transition:"all .5s",
-            transformOrigin:"right",
+            transition: "all .5s",
+            transformOrigin: "right",
         },
         '&::before': {
             content: "''",
             position: "absolute",
             backgroundColor: "white",
             top: "0",
-            left:"0",
+            left: "0",
             width: "100%",
             height: ".25rem",
             display: "inline-block",
             transform: "scaleX(0)",
-            transition:"all .5s",
-            transformOrigin:"left",
+            transition: "all .5s",
+            transformOrigin: "left",
         },
         '&:hover::after': {
             transform: "scaleX(1)",
-
         },
         '&:hover::before': {
             transform: "scaleX(1)",
+        },
+    },
+    '@media screen and (max-width: 610px)': {
+        header: {
+            backgroundColor: "white",
 
         },
-
+        nav_ul: {
+            display: "flex",
+            flexDirection: "column",
+        }
     },
+    side_menu: {
+        width: "20rem",
+        height: "100vh",
+        float: "right",
+        backgroundColor: "var(--secondary-color)",
+        display: "flex",
+        flexDirection: "column",
+    },
+    close_button_layer: {
+        display: "flex",
+        justifyContent: "flex-end",
+    },
+    rest_of_menu: {
+        flexGrow:"1",
+        backgroundColor:"teal",
+    },
+    menu_close_btn: {
+        color:"white",
+        fontSize: "4rem",
+        margin: "2rem 3rem 0rem 0rem",
+        cursor: "pointer",
+    },
+    nav_ul_menu:{
+        flexGrow:"1",
+        display:"flex",
+        flexDirection:"column",
+        justifyContent:"center",
+        alignItems:"center",
+        listStyle:"none",
+        color:"white",
+        '& li':{
+            margin:"2rem",
+        }
+
+    }
 
 
 })
 
 function Home() {
+    const theme = useTheme()
+    const matches = useMediaQuery(theme.breakpoints.down("sm"));
+    const [showMenu, setShowMenu] = useState(false)
     const classes = useStyles()
+    const menuTapHandler = (e) => {
+        if (showMenu) {
+            setShowMenu(false)
+        } else {
+            setShowMenu(true)
+        }
+    }
     return (
         <React.Fragment>
             <Head>
@@ -127,10 +201,6 @@ function Home() {
                 {/* Side Nav */}
                 <div className={classes.side_nav}>
                     <div className={classes.side_nav_content}>
-                        <div className={classes.side_nav_content_logo}>
-                            <Image src={LogoImage} className={classes.logo_image}
-                                   alt={"logo"} objectFit={"cover"}/>
-                        </div>
                         <ul className={classes.social_ul}>
                             <a className={classes.social_button} href="#">
                                 <FontAwesomeIcon icon={faFacebookF}/></a>
@@ -143,14 +213,8 @@ function Home() {
                 </div>
                 {/* Main Nav */}
                 <main className={classes.main}>
-                    <header className={classes.header}>
+                    {!matches && <header className={classes.header}>
                         <nav className={classes.header_nav}>
-                            {/*<div className={classes.main_nav_hamburger}>*/}
-                            {/*    <div className={`${classes.line} ${classes.line1}`}>Home</div>*/}
-                            {/*    <div className={`${classes.line} ${classes.line2}`}>Home</div>*/}
-                            {/*    <div className={`${classes.line} ${classes.line3}`}>Home</div>*/}
-                            {/*    <div className={`${classes.line} ${classes.line4}`}>Home</div>*/}
-                            {/*</div>*/}
                             <ul className={classes.nav_ul}>
                                 <li><a className={classes.nav_btn} href="#">Home</a></li>
                                 <li><a className={classes.nav_btn} href="#">About</a></li>
@@ -159,7 +223,27 @@ function Home() {
                                 <li><a className={classes.nav_btn} href="#">Hire me</a></li>
                             </ul>
                         </nav>
-                    </header>
+                    </header>}
+                    {matches && <div className={classes.header}>
+                        {!showMenu && <div className={classes.main_nav_hamburger} onClick={menuTapHandler}>
+                            <div className={classes.line}/>
+                            <div className={classes.line}/>
+                            <div className={classes.line}/>
+                        </div>}
+                        {showMenu && <div className={classes.side_menu}>
+                            <div className={classes.close_button_layer}>
+                                <ClearIcon className={classes.menu_close_btn}
+                                      onClick={menuTapHandler} fontSize={"3rem"}/>
+                            </div>
+                                <ul className={classes.nav_ul_menu}>
+                                    <li><a className={classes.nav_btn} href="#">Home</a></li>
+                                    <li><a className={classes.nav_btn} href="#">About</a></li>
+                                    <li><a className={classes.nav_btn} href="#">Projects</a></li>
+                                    <li><a className={classes.nav_btn} href="#">Services</a></li>
+                                    <li><a className={classes.nav_btn} href="#">Hire me</a></li>
+                                </ul>
+                        </div>}
+                    </div>}
 
                 </main>
             </div>
